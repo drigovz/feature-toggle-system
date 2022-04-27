b464659b9cc801a463bff680928f7850f37e6c45
Rodrigo Vaz <rodrigodp2014@gmail.com>
2022-04-26 21:36:53 -0300

    feat: add entities

---
+++ BackEnd/src/FeatureToggleSystem.Domain/Entities/FeatureFlag.cs
@@ -0,0 +1,25 @@
+﻿namespace FeatureToggleSystem.Domain.Entities
+{
+    public sealed class FeatureFlag : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool Active { get; private set; } = false;
+
+        public FeatureFlag(string name, string description, bool active)
+        {
+            Name = name;
+            Description = description;
+            Active = active;
+        }
+
+        public FeatureFlag Update(string name, string description, bool active)
+        {
+            Name = name;
+            Description = description;
+            Active = active;
+
+            return this;
+        }
+    }
+}
